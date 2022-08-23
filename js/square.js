8ef03bedccf0ca812ad0498a4ac5278566080614
juliuszzzxc <110211932+juliuszzzxc@users.noreply.github.com>
2022-08-23 21:52:17 +0800

    rectangle and square

---
+++ js/square.js
@@ -0,0 +1,11 @@
+function area(){
+    let num1 = parseFloat(document.getElementById("length").value);
+    let num2 = parseFloat(document.getElementById("width").value);
+    let namo = parseFloat(num1 * num2);
+    let namo2= parseFloat(num1 + num2)
+    
+     document.getElementById("namo1").innerHTML= "Area of Square: "+namo;
+     document.getElementById("namo2").innerHTML="Parameter Of Square: "+ namo2;
+    console.log(namo)
+
+}
