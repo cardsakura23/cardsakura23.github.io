53b1012793321a96d6a3d16c623cee2a3cc4a85d
cardsakura23 <phyllip.sam.salvador23@gmail.com>
2022-08-23 14:09:36 +0400

    uploaded html code in js files

--- Sector.js
+++ Sector.js
@@ -1,3 +1,14 @@
+// html
+// <p>Sector</p>
+// <label for="sectorCentralAngle">Central Angle Degree</label>
+// <input type="number" name="sectorDegree" id="sectorDegree"><br>
+// <label for="sectorRadius">Radius</label>
+// <input type="number" name="sectorRadius" id="sectorRadius"><br>
+// <p>"The area of the sector is: "<span id="sectorArea"></span></p>
+// <p>"The circumference of the sector is: "<span id="sectorPerimeter"></span></p>
+// <button onclick="sector()">Calculate</button><br>
+
+
 function sector(){
     var sectorDegreeOne = parseInt(document.getElementById('sectorDegree').value);
     var sectorRadiusOne = parseInt(document.getElementById('sectorRadius').value);
