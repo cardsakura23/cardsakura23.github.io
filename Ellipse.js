fc12867d6a1abb82a865e2848dae42618f5eab6a
unknown <madduma.jannfraulien@gmail.com>
2022-08-24 00:00:42 +0800

    linked js to html see ellipse

--- Ellipse.js
+++ Ellipse.js
@@ -8,13 +8,20 @@
 //     <p>"The circumference of the ellipse is: "<span id="ellipsePerimeter"></span></p>
 //     <button onclick="ellipse()">Calculate</button><br></br>
 
+//
+function ellipse() {
+  var ellipseAxis1 = parseInt(document.getElementById("ellipseAxisOne").value);
+  var ellipseAxis2 = parseInt(document.getElementById("ellipseAxisTwo").value);
+  var ellipseArea = Math.PI * ellipseAxis1 * ellipseAxis2;
+  var ellipsePerimeter =
+    Math.sqrt((ellipseAxis1 * ellipseAxis1 + ellipseAxis2 + ellipseAxis2) / 2) *
+    2 *
+    Math.PI;
+  console.log(ellipseArea);
+  console.log(ellipsePerimeter);
 
-// 
-function ellipse(){
-    var ellipseAxis1 = parseInt(document.getElementById('ellipseAxisOne').value);
-    var ellipseAxis2 = parseInt(document.getElementById('ellipseAxisTwo').value);
-    var ellipseArea = (Math.PI*ellipseAxis1*ellipseAxis2);
-    var ellipsePerimeter = (Math.sqrt((ellipseAxis1*ellipseAxis1+ellipseAxis2+ellipseAxis2)/2)*2*Math.PI)
-    console.log(ellipseArea);
-    console.log(ellipsePerimeter);
+  document.getElementById("resultArea").innerHTML = ellipseArea;
+  document.getElementById("resultPerimeter").innerHTML = ellipsePerimeter;
+  document.getElementById("ellipseAxisOneValue").innerHTML = ellipseAxis1;
+  document.getElementById("ellipseAxisTwoValue").innerHTML = ellipseAxis2;
 }
