77fbeb852178d8738c42079720c195ec850582a2
unknown <madduma.jannfraulien@gmail.com>
2022-08-24 11:25:13 +0800

    updated

--- rhombus.js
+++ rhombus.js
@@ -1,21 +1,26 @@
-function areaRhombus(){
-    var p=parseInt(documentGetElementbyId("firstDiagonal").value);
-    var q=parseInt(documentGetElementbyId("secondDiagonal").value);
+function areaRhombus() {
+  var p = parseInt(documentGetElementbyId("firstDiagonal").value);
+  var q = parseInt(documentGetElementbyId("secondDiagonal").value);
 
-    var area=(p*q)/2;
-    return area();
+  var area = (p * q) / 2;
+  return area();
+  document.getElementById("resultArea").innerHTML = area;
+  document.getElementById("firstDiagonalValue").innerHTML = p;
+  document.getElementById("secondDiagonalValue").innerHTML = q;
 }
 
-function perimRhombus(){
-    var a=parseInt(documentGetElementbyId("side").value);
-    var peri=4*a;
-    
-    return perimRhombus();
+function perimRhombus() {
+  var a = parseInt(documentGetElementbyId("side").value);
+  var peri = 4 * a;
+
+  return perimRhombus();
+  document.getElementById("resultPerimeter").innerHTML = peri;
+  document.getElementById("rhombusArea").innerHTML = a;
 }
 
 function buttonRhombus() {
-    areaRhombus();
-    perimRhombus();
+  areaRhombus();
+  perimRhombus();
 
-    return buttonRhombus();
+  return buttonRhombus();
 }
